ff97763aa377dfec58e1e82f5c32ff13bfb06ccd
elina <elina.kalnina@gmail.com>
2024-02-05 16:17:49 +0200

    Add project files.

---
+++ HelloWorld/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Customer
+    {
+        public Customer(string n, string s)
+        {
+            Name = n;
+            Surname = s;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string FullName { get {
+                return Name +" " + Surname; 
+            } }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter name");
+string n = "";
+string s = "";
+n= Console.ReadLine();
+Console.WriteLine("Enter surname");
+s= Console.ReadLine();
+
+Customer c = new Customer(s, n);
+Console.WriteLine("Full name: " + c.FullName);
+Console.ReadLine();
+
+//namespace MyProject;
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Hello, World!");
+//    }
+//}
